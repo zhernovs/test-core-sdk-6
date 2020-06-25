66c129f428496a8eb44a76f37a3ef6d43f7c8168
zhernovs <ext-andriy.zhernovskyi@here.com>
2020-06-25 10:00:14 +0300

    generate config with script

---
+++ generate-config.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "trigger_internal_ci:
+  stage: trigger
+  variables:
+    EXT_PULL_REQUEST_TARGET_BRANCH_NAME: $CI_EXTERNAL_PULL_REQUEST_TARGET_BRANCH_NAME
+    EXT_PULL_REQUEST_SOURCE_BRANCH_NAME: $CI_EXTERNAL_PULL_REQUEST_SOURCE_BRANCH_NAME
+    EXT_PULL_REQUEST_IID: $CI_EXTERNAL_PULL_REQUEST_IID
+    TRIGGER_BRANCH: $TRIGGER_BRANCH
+  trigger:
+    project: zhernovs/test-sdk-6
+    strategy: depend
+    branch: $TRIGGER_BRANCH" > generated-config.yml
