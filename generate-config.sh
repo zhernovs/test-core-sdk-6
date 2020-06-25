2371149054958fd97d86dbc9a8855f93e396edfb
zhernovs <ext-andriy.zhernovskyi@here.com>
2020-06-25 11:20:41 +0300

    raise bridge job from within autogenerated child pipeline

--- generate-config.sh
+++ generate-config.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
-# echo "trigger_internal_ci:
-#   stage: trigger
-#   variables:
-#     EXT_PULL_REQUEST_TARGET_BRANCH_NAME: $CI_EXTERNAL_PULL_REQUEST_TARGET_BRANCH_NAME
-#     EXT_PULL_REQUEST_SOURCE_BRANCH_NAME: $CI_EXTERNAL_PULL_REQUEST_SOURCE_BRANCH_NAME
-#     EXT_PULL_REQUEST_IID: $CI_EXTERNAL_PULL_REQUEST_IID
-#     TRIGGER_BRANCH: $TRIGGER_BRANCH
-#   trigger:
-#     project: zhernovs/test-sdk-6
-#     strategy: depend
-#     branch: $TRIGGER_BRANCH" > generated-config.yml
-
-
 cat > generated-config.yml <<HEREDOC
-test_inherited_var_feature:
-  script:
-    - echo "VAR $TRIGGER_BRANCH"
+trigger_internal_ci:
+  variables:
+    EXT_PULL_REQUEST_TARGET_BRANCH_NAME: $CI_EXTERNAL_PULL_REQUEST_TARGET_BRANCH_NAME
+    EXT_PULL_REQUEST_SOURCE_BRANCH_NAME: $CI_EXTERNAL_PULL_REQUEST_SOURCE_BRANCH_NAME
+    EXT_PULL_REQUEST_IID: $CI_EXTERNAL_PULL_REQUEST_IID
+    TRIGGER_BRANCH: $TRIGGER_BRANCH
+  trigger:
+    project: zhernovs/test-sdk-6
+    strategy: depend
+    branch: $TRIGGER_BRANCH
 HEREDOC
 
-
+# cat > generated-config.yml <<HEREDOC
+# test_inherited_var_feature:
+#   script:
+#     - echo "VAR $TRIGGER_BRANCH"
+# HEREDOC
