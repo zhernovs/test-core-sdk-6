ba271e935c49959f9f76da52bfe4ef5be38b69ff
zhernovs <ext-andriy.zhernovskyi@here.com>
2020-06-25 10:12:47 +0300

    simple child job

--- generate-config.sh
+++ generate-config.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
-echo "trigger_internal_ci:
-  stage: trigger
-  variables:
-    EXT_PULL_REQUEST_TARGET_BRANCH_NAME: $CI_EXTERNAL_PULL_REQUEST_TARGET_BRANCH_NAME
-    EXT_PULL_REQUEST_SOURCE_BRANCH_NAME: $CI_EXTERNAL_PULL_REQUEST_SOURCE_BRANCH_NAME
-    EXT_PULL_REQUEST_IID: $CI_EXTERNAL_PULL_REQUEST_IID
-    TRIGGER_BRANCH: $TRIGGER_BRANCH
-  trigger:
-    project: zhernovs/test-sdk-6
-    strategy: depend
-    branch: $TRIGGER_BRANCH" > generated-config.yml
+# echo "trigger_internal_ci:
+#   stage: trigger
+#   variables:
+#     EXT_PULL_REQUEST_TARGET_BRANCH_NAME: $CI_EXTERNAL_PULL_REQUEST_TARGET_BRANCH_NAME
+#     EXT_PULL_REQUEST_SOURCE_BRANCH_NAME: $CI_EXTERNAL_PULL_REQUEST_SOURCE_BRANCH_NAME
+#     EXT_PULL_REQUEST_IID: $CI_EXTERNAL_PULL_REQUEST_IID
+#     TRIGGER_BRANCH: $TRIGGER_BRANCH
+#   trigger:
+#     project: zhernovs/test-sdk-6
+#     strategy: depend
+#     branch: $TRIGGER_BRANCH" > generated-config.yml
+
+echo "test_inherited_var_feature:
+ stage: trigger
+ script:
+   - echo \"VAR $TRIGGER_BRANCH\" 
+ needs:
+   - job: check_branch_existance
+     artifacts: true" > generated-config.yml
