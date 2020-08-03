f2acf1c6aa27642b7af9c09d33da8a526466070b
aluni <gairik.aluni@here.com>
2020-08-03 13:27:26 +0200

    Changing token name to SVC_HLS_RENDER_API

--- test/rendering/StylingTest.ts
+++ test/rendering/StylingTest.ts
@@ -746,7 +746,7 @@ describe("MapView Styling Test", function() {
                 coordinates: [
                     [
                         [0.004, 0.002],
-                        [-0.006, 0.002],
+                        [-0.007, 0.002],
                         [-0.005, -0.002],
                         [0.004, -0.002]
                     ]
